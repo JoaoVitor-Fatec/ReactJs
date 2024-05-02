f110c162df0d1bf0eed3333af5503d5e43af1099
Joao Moreno <161522359+JoaoVitor-Fatec@users.noreply.github.com>
2024-05-01 21:25:53 -0300

    Update Relogio.js

--- src/Relogio.js
+++ src/Relogio.js
@@ -1,6 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
+
 function Relogio() {
+  const [horarioAtual, setHorarioAtual] = useState('');
+
+  useEffect(() => {
+    const updateHorario = () => {
+      const dataAtual = new Date();
+      const hour = formatValor(dataAtual.getHours());
+      const minutes = formatValor(dataAtual.getMinutes());
+      const seconds = formatValor(dataAtual.getSeconds());
+
+      setHorarioAtual(`Hora atual: ${hour}:${minutes}:${seconds}`);
+    };
+
+    const formatValor = (valor) => {
+      return valor < 10 ? "0" + valor : valor;
+    };
+
+    const intervalId = setInterval(updateHorario, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
-    <div>Meu Relógio</div>
+    <div>
+      <p id="horarioAtual">{horarioAtual}</p>
+    </div>
   );
 }
+
 export default Relogio;
