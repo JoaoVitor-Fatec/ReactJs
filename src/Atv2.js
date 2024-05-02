2ede8759d34b36922c741ce5e46fd6f74699ee91
Joao Moreno <161522359+JoaoVitor-Fatec@users.noreply.github.com>
2024-05-01 22:49:41 -0300

    Add files via upload

--- src/Atv2.js
+++ src/Atv2.js
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 
 export default function Atv2() {
   return (
-    <h1>Atividade 2</h1>
+    <>
+      <h1>Atividade 2</h1>
+      <Link to="/">Voltar</Link>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import MinhasRotas from './minhasRotas';
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <MinhasRotas />
