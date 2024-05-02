4b13d6b7ee4a8bd5fc50ed759ce0bee396190dbf
Joao Moreno <161522359+JoaoVitor-Fatec@users.noreply.github.com>
2024-05-01 21:27:18 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Letreiro from './Letreiro';
-import Relogio from './Relogio';
+import MinhasRotas from './minhasRotas';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <Letreiro />
-    <Relogio />
-  </>
+  <MinhasRotas />
 );
+
