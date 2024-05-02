330d8902a3e32a8902514ba24b05f354c2d16f7c
Joao Moreno <161522359+JoaoVitor-Fatec@users.noreply.github.com>
2024-05-01 21:25:32 -0300

    Update Letreiro.js

--- src/Letreiro.js
+++ src/Letreiro.js
@@ -1,6 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
 function Letreiro() {
-    return (
-      <div>Meu Letreiro</div>
-    );
-  }
-  export default Letreiro;
+  const [texto, setTexto] = useState('');
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const drawTexto = () => {
+      const textoComp = 'Conheça a Fatec!';
+      if (index < textoComp.length) {
+        setTexto(texto + textoComp[index]);
+        setIndex(index + 1);
+      } else {
+        setTexto('');
+        setIndex(0);
+      }
+    };
+
+    const timerID = setInterval(() => {
+      drawTexto();
+    }, 100);
+
+    return () => clearInterval(timerID);
+  }, [index, texto]);
+
+  return <div>{texto}</div>;
+}
+
+export default Letreiro;
+
+
+
+
